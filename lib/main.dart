2652a8d9824c824daf50c4ff82c81d5864bfb2ad
amytang <27tangamy@gmail.com>
2025-08-30 14:32:10 -0400

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:stock_market_appp/analysis.dart';
 import 'package:stock_market_appp/home_page.dart';
-import 'package:stock_market_appp/portfolio.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
